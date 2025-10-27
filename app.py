3a367810116395de4b468d0fc5864c2208f759c3
chaitanyaprasad-p45 <chaitanyaprasad.p45@gmail.com>
2025-10-27 21:36:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS via Jenkins!"
+    return "Hello from ECS (Docker) BLA BLA BLA!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
